ffda6cc1e5bb19a80afe4f32fe81cd89692ed42a
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-09 10:30:18 +0300

    Added fields, lombok and hibernate annotations

--- src/main/java/com/entity/Actor.java
+++ src/main/java/com/entity/Actor.java
@@ -1,4 +1,28 @@
 package com.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="actors")
 public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long actorId;
+    private String firstName;
+    private String lastName;
+    @ManyToMany
+    @JoinTable(name="actor_film",
+               joinColumns={@JoinColumn(name="actorId")},
+               inverseJoinColumns = {@JoinColumn(name="filmId")})
+    private Set<Film> films;
 }
