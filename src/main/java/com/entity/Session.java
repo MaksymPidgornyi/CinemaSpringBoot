19f94ea4c11e585d8ab1a0ea05905fdc9e92530f
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-09 10:50:03 +0300

    added fields, added lombok and hibernate annotations

--- src/main/java/com/entity/Session.java
+++ src/main/java/com/entity/Session.java
@@ -1,4 +1,23 @@
 package com.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name="sessions")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long sessionId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Film film;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate sessionDate;
 }
