93edb7e6766259b271e2980225e954a2c6f3284d
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-09 11:16:13 +0300

    added fields, added lombok and hibernate annotations

--- src/main/java/com/entity/Ticket.java
+++ src/main/java/com/entity/Ticket.java
@@ -1,4 +1,21 @@
 package com.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="tickets")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ticketId;
+    private short placeNumber;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Session session;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
 }
