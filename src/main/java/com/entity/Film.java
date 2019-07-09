59b4b6a555e7118adead24336c173e2adf3ebd76
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-09 10:42:21 +0300

    added fields, added lombok and hibernate annotations

--- src/main/java/com/entity/Film.java
+++ src/main/java/com/entity/Film.java
@@ -1,4 +1,22 @@
 package com.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.Year;
+
+@Entity
+@Table(name="films")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long filmId;
+    private String filmName;
+    private String filmGenre;
+    private Year created;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Director director;
 }
