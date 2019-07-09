f97b66db37c28457d8feeab79f1026de11dc1100
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-09 10:36:36 +0300

    added fields, added lombok and hibernate annotations

--- src/main/java/com/entity/Director.java
+++ src/main/java/com/entity/Director.java
@@ -1,4 +1,21 @@
 package com.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="directors")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long directorId;
+    private String firstName;
+    private String lastName;
+    @OneToMany(fetch = FetchType.EAGER, mappedBy="filmId")
+    private Set<Film> films;
 }
