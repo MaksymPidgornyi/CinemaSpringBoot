f3e13a754fff68c647300706b57967d9eb83ab64
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-10 20:31:53 +0300

    implemented getLogin method

--- src/main/java/com/controller/AppController.java
+++ src/main/java/com/controller/AppController.java
@@ -1,11 +1,17 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/cinema")
 public class AppController {
     
     public void getMainPage(){
 
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login.ftl";
     }
 }
