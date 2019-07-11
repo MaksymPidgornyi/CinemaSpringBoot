d2a7202fb5d555fedae863c2238aab9f366ca034
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-11 11:10:47 +0300

    added fields

--- src/main/java/com/model/dto/UserDto.java
+++ src/main/java/com/model/dto/UserDto.java
@@ -1,4 +1,13 @@
 package com.model.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDto {
+    private long id;
+    private String login;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
 }
