d9796ebfafe11d91bec8da5b394c04a6fe26f4d7
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-12 12:04:25 +0300

    implemented postRegister method

--- src/main/java/com/controller/AppController.java
+++ src/main/java/com/controller/AppController.java
@@ -1,11 +1,26 @@
 package com.controller;
 
+import com.model.dto.UserDto;
+import com.model.entity.*;
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("/")
 public class AppController {
+
+    private UserService userService;
+    private MessageSource messageSource;
+
+    @Autowired
+    public AppController(UserService userService, MessageSource messageSource) {
+        this.userService = userService;
+        this.messageSource = messageSource;
+    }
+
     
     public void getMainPage(){
 
@@ -19,6 +34,18 @@ public class AppController {
     @GetMapping("/register")
     public String getRegister(){
         return "register";
+    }
+
+    @PostMapping("/register")
+    public void postRegister(@RequestBody UserDto dto){
+
+        System.out.println(dto);
+
+        User user = userService.dtoToUser(dto);
+
+        System.out.println(user);
+
+        userService.createUser(user);
     }
 
 }
