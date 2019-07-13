0c6632fb577912161cfb4e316bcd0013ac4ea920
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-13 08:15:39 +0300

    implemented addActor method

---
+++ src/main/java/com/service/ActorService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import com.model.entity.Actor;
+import com.model.repository.ActorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActorService {
+    private ActorRepository repository;
+
+    @Autowired
+    public void setRepository(ActorRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addActor(Actor actor){
+        repository.save(actor);
+    }
+}
