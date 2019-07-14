caa3c28390c6119abfa7c7810282488c00ac2ea9
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-14 11:41:30 +0300

    implemented getAllDirectors method

--- src/main/java/com/service/DirectorService.java
+++ src/main/java/com/service/DirectorService.java
@@ -1,7 +1,24 @@
 package com.service;
 
+import com.model.entity.Director;
+import com.model.repository.DirectorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DirectorService {
+    private DirectorRepository repository;
+
+    @Autowired
+    public void setRepository(DirectorRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addDirector(Director director){
+        repository.save(director);
+    }
+
+    public Iterable<Director> getAllDirectors(){
+        return repository.findAll();
+    }
 }
