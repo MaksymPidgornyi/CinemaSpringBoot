30dff1aedf39051fc36e54b235f9d67ce1fc23c5
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-15 14:57:01 +0300

    added custom resource handler

--- src/main/java/com/configuration/WebMvcConfig.java
+++ src/main/java/com/configuration/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.*;
 
@@ -30,5 +31,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources");
     }
 }
