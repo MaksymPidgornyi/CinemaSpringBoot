e6f96d0278957245870ab9835adb866e5c558d92
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-15 11:42:33 +0300

    added validation

--- src/main/java/com/model/entity/Actor.java
+++ src/main/java/com/model/entity/Actor.java
@@ -1,8 +1,11 @@
 package com.model.entity;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.web.servlet.LocaleResolver;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Entity
@@ -12,7 +15,11 @@ public class Actor {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long actorId;
+    @NotBlank(message = "message.error.first.name")
+    @Length(max = 30, message = "message.error.toolong")
     private String firstName;
+    @NotBlank(message = "message.error.last.name")
+    @Length(max = 30, message = "message.error.toolong")
     private String lastName;
     @ManyToMany
     @JoinTable(name="actor_film",
