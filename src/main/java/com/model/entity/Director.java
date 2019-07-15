3926c1d94e5cfdaf08d665095d445bdf6ab09ed1
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-15 11:42:46 +0300

    added validation

--- src/main/java/com/model/entity/Director.java
+++ src/main/java/com/model/entity/Director.java
@@ -1,8 +1,10 @@
 package com.model.entity;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Entity
@@ -14,7 +16,11 @@ public class Director {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long directorId;
+    @NotBlank(message = "message.error.first.name")
+    @Length(max = 30, message = "message.error.toolong")
     private String firstName;
+    @NotBlank(message = "message.error.first.name")
+    @Length(max = 30, message = "message.error.toolong")
     private String lastName;
     @OneToMany(fetch = FetchType.EAGER, mappedBy="director")
     private Set<Film> films;
