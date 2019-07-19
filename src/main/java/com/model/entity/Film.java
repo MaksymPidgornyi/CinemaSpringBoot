f49096c6fc52a32dc313710d334941f7536e8e3a
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-19 15:30:56 +0300

    added field validation

--- src/main/java/com/model/entity/Film.java
+++ src/main/java/com/model/entity/Film.java
@@ -4,9 +4,10 @@ import com.model.entity.enums.Genre;
 import lombok.*;
 
 import javax.persistence.*;
-import java.time.LocalTime;
-import java.time.Year;
+import javax.validation.constraints.*;
+import java.time.*;
 import java.util.Set;
+
 
 @Entity
 @Table(name="films")
@@ -15,20 +16,27 @@ public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long filmId;
+    @NotBlank
     private String filmName;
     @ElementCollection(targetClass = Genre.class, fetch = FetchType.EAGER)
     @CollectionTable(name="film_genre",
                 joinColumns={@JoinColumn(name="film_id")}
     )
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Set<Genre> filmGenre;
-
+    @NotNull
+    @PastOrPresent
     private Year created;
+    @NotNull
     private LocalTime duration;
     @ManyToOne(fetch = FetchType.EAGER)
+    @NotNull
     private Director director;
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "film")
     private Set<Session> sessions;
     @ManyToMany(mappedBy = "films", fetch = FetchType.EAGER)
+    @NotNull
+    @NotEmpty
     private Set<Actor> actors;
 }
