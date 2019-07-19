43a6adb6aa81b385557ded1ed22ae089bb6545af
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-19 15:34:13 +0300

    added encodePassword method, removed dto to user method

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -1,43 +1,30 @@
 package com.service;
 
-import com.model.dto.UserDto;
-import com.model.entity.Role;
 import com.model.entity.User;
 import com.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserRepository rep;
     private MessageSource messageSource;
+    private PasswordEncoder encoder;
 
     @Autowired
-    public UserService(UserRepository rep, MessageSource messageSource) {
+    public UserService(UserRepository rep, MessageSource messageSource, PasswordEncoder encoder) {
         this.rep = rep;
         this.messageSource = messageSource;
+        this.encoder = encoder;
     }
 
-    public User dtoToUser(UserDto dto){
-        User user = new User();
-
-        user.setFirstName(dto.getFirstName());
-        user.setLastName(dto.getLastName());
-        user.setLogin(dto.getLogin());
-        user.setEmail(dto.getEmail());
-        user.setRole(Role.USER);
-        user.setPassword(dto.getPassword());
-        user.setActivity(true);
-
-        return user;
+    public String encodePassword(String password){
+        return encoder.encode(password);
     }
 
     public void createUser(User user){
         rep.save(user);
-    }
-
-    public boolean isDtoValid(UserDto dto){
-        return false;
     }
 }
