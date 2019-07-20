1dadc20d0c96555f14a8fd35f3471886adb97ec3
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-20 13:02:28 +0300

    initial commit

--- src/main/java/com/service/SessionService.java
+++ src/main/java/com/service/SessionService.java
@@ -1,7 +1,15 @@
 package com.service;
 
+import com.model.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionService {
+    private SessionRepository repository;
+
+    @Autowired
+    public void setRepository(SessionRepository repository) {
+        this.repository = repository;
+    }
 }
