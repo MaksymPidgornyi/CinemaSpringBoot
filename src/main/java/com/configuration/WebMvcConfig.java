5feb9e98598ef7f575c3473e87442e901c2b0b07
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-20 12:58:10 +0300

    added beans for i18n, overriden addInterceptors and addResourceHandlers method

--- src/main/java/com/configuration/WebMvcConfig.java
+++ src/main/java/com/configuration/WebMvcConfig.java
@@ -1,16 +1,15 @@
 package com.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.*;
 
 import java.util.Locale;
 
 @Configuration
+@EnableSpringDataWebSupport
 public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver(){
