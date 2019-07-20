6079b2f46bf6846ccd631681dda5a7d3f403ee43
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-20 14:28:15 +0300

    implemented postSession method

--- src/main/java/com/controller/SessionsController.java
+++ src/main/java/com/controller/SessionsController.java
@@ -1,13 +1,20 @@
 package com.controller;
 
+import com.model.entity.Session;
 import com.service.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.util.Map;
 import java.util.Objects;
 
 @Controller
@@ -27,6 +34,28 @@ public class SessionsController {
 
         model.addAttribute("sessions", sessionService.getSessionsByDate(sessionsDate, pageable));
         model.addAttribute("sessionsDate", sessionsDate);
+        model.addAttribute("maxSessionStartTime", LocalTime.of(22, 0));
+
+        return "afisha";
+    }
+
+    @PostMapping("/session")
+    public String postSession(@Valid Session session, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrorsMap(bindingResult);
+
+            model.mergeAttributes(errors);
+        }
+        else {
+
+            LocalTime start = session.getStartTime();
+            Duration movieDuration = session.getFilm().getDuration();
+
+            session.setEndTime(start.plus(movieDuration));
+
+            sessionService.create(session);
+        }
 
         return "afisha";
     }
