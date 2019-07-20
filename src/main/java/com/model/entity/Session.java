efd915c80f52a1feb52b3a7e0f2f7ff5cf939e4f
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-07-20 13:17:32 +0300

    added validation

--- src/main/java/com/model/entity/Session.java
+++ src/main/java/com/model/entity/Session.java
@@ -3,23 +3,25 @@ package com.model.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
 
 @Entity
 @Table(name="sessions")
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long sessionId;
     @ManyToOne(fetch = FetchType.EAGER)
+    @NotNull
     private Film film;
+    @NotNull
     private LocalTime startTime;
     private LocalTime endTime;
+    @NotNull
     private LocalDate sessionDate;
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "session")
     private Set<Ticket> tickets;
