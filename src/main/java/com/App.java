e799649a4f85afc4132e2ab25d93007a1b6c24eb
MaksymPidgornyi <45099268+MaksymPidgornyi@users.noreply.github.com>
2019-08-14 00:54:36 +0300

    initial commit

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -2,9 +2,13 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.model.repository")
 public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
